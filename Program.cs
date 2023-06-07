269651fcd0952b52e397f110303f8c2eda0790ae
VictorH-D-S <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-07 14:34:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auls01Textos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -> \N = PULA A LINHA
+            // Console.WriteLine(""); MOSTRA TEXTO NA TELA E ROLA DE ESCREVER O QUANTO TU QUISER TÁ LIGADO
+            // SEMPRE USAR ; NO FINAL
+
+            Console.WriteLine("O inimigo mais mortal que você pode encontrar é você mesmo, esse é o preço da consciência de si mesmo.\n \n");
+           // CACHORRO CHUPETÃO VEM FORTE PARA O MELHOR MEME DE 2023
+
+            //A BARRA INVERTIDA \ SERVE PARA USAR ASPAS SEM AFETAR O TEXTO
+            Console.WriteLine("O que disseste \"cachorro chupetão\"? \n \n");
+            Console.WriteLine("Tuc tuc tuc tuc \n \n");
+           
+            Console.WriteLine("Cachorro Chupetinha (filho dele) \n \n");
+            Console.WriteLine("\n \n \n \n \n \n"); //USANDO MAIS '\N', PULA-SE MAIS LINHAS
+            //KKKKKKKKKKKKKKKKKKKK
+            Console.Write("Restaurante do Cachorro Chupetão \n \n");
+            Console.WriteLine("Hoje temos: \n" +
+                "- Cafézinho com goiaba e alvejante R$ 33,33 \n" +
+                "- Arroz de semana passada com uva passa e maçã R$ 69,99 \n" +
+                "- Cuzcuz paulista R$ 16,50 \n \n \n" + //CUZCUZ PAULISTA MELHOR DO MUNDO
+                "Escolha sua opção, coma, pague e cale a maldita boca.");
+
+                
+            //O COMANDO ABAIXO SERVER PARA "PAUSAR" O CONSOLE
+            Console.ReadKey();
+            //O COMANDO ACIMA SERVER PARA "PAUSAR" O CONSOLE
+           
+            //Eu estou ficando louco :D
+            //O JOKER O PALHÇAO PRINCIPE DO CRIME O CORINGA O PALHAÇO
+            //BATMAN DA ONDE VOCÊ TIROU ESSE BATESCUDO EM?
+
+        }
+    }
+}
